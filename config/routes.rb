48e46fb7f51972f845569c460449446b7319e0ac
fperez371 <francisco.e.perez@gmail.com>
2019-01-22 18:27:18 -0500

    change error

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: json } do
     resources :users
-    session :session, only [:create, :show, :destroy]
+    resource :session, only: [:create, :show, :destroy]
   end
 end
