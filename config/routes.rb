275adca3fa036d36ace8cf0519a67f063bbb85bb
fperez371 <francisco.e.perez@gmail.com>
2019-01-30 17:42:57 -0500

    add project routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users
     resource :session, only: [:create, :show, :destroy]
+    resources :projects
   end
 end
