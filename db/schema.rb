61308f1a543b43fe1f6b9d520106476ee3a33dd5
fperez371 <francisco.e.perez@gmail.com>
2019-02-18 15:20:52 -0500

    add location column to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_161915) do
+ActiveRecord::Schema.define(version: 2019_02_18_163622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_02_18_161915) do
     t.datetime "deadline", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location", null: false
     t.index ["creator_id"], name: "index_projects_on_creator_id"
   end
 
