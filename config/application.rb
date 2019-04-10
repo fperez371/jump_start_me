4bbf783fd42aa48653814174ad9666587a742bf1
fperez371 <francisco.e.perez@gmail.com>
2019-04-10 11:02:43 -0400

    revert changes to config file

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module JumpStartMe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.initialize_on_precompile = false
-
     # config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
