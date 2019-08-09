e996ee4c76758768eefa487651ca2780e45d77a9
fperez371 <francisco.e.perez@gmail.com>
2019-08-09 18:41:25 -0400

    move handleSubmit binding to search constructor to avoid excessive rebinding, minor css change on homepage to change cursor to pointer when user avatar link is hovered, README changes

---
+++ README.md
@@ -0,0 +1,28 @@
+# JumpStartMe
+
+JumpStartMe is a web application inspired by Kickstarter.
+
+[Visit the Live JumpStartMe App here](https://jumpstartme.herokuapp.com/#/)
+
+---
+
+## Features
+
+-   User Authentication
+-   Home Page
+    -   Featured Project
+    -   Recommended Projects
+-   Start a project
+    -   Create a project with a funding goal, rewards, a deadline, etc. just like on Kickstarter!
+
+## Technology Used
+
+-   Ruby on Rails (backend)
+    -   ActiveRecord
+    -   ActiveStorage (AWS)
+    -   React/Redux (frontend)
+    -   PostgreSQL
+    -   User Authentication
+    -   jQuery
+    -   DOM Manipulation
+    -   ES6
--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -2,113 +2,116 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import {
-  fetchSearchResults,
-  receiveSearchResults,
+    fetchSearchResults,
+    receiveSearchResults,
 } from "../../actions/search_actions";
 
 const msp = state => {
-  let results = state.entities.results || null;
-  return {
-    results: results,
-  };
+    let results = state.entities.results || null;
+    return {
+        results: results,
+    };
 };
 
 const mdp = dispatch => {
-  return {
-    fetchResults: query => dispatch(fetchSearchResults(query)),
-    clearResults: () => dispatch(receiveSearchResults({})),
-  };
+    return {
+        fetchResults: query => dispatch(fetchSearchResults(query)),
+        clearResults: () => dispatch(receiveSearchResults({})),
+    };
 };
 
 class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: "",
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: "",
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  edit(query) {
-    return e =>
-      this.setState({
-        [query]: e.currentTarget.value,
-      });
-  }
+    edit(query) {
+        return e =>
+            this.setState({
+                [query]: e.currentTarget.value,
+            });
+    }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    let query = this.state.query;
-    if (query.length < 1) return;
-    this.props.fetchResults(query).then(() => {
-      this.props.toggleSearch;
-    });
-  }
+    handleSubmit(e) {
+        e.preventDefault();
+        let query = this.state.query;
+        if (query.length < 1) return;
+        this.props.fetchResults(query).then(() => {
+            this.props.toggleSearch;
+        });
+    }
 
-  componentWillUnmount() {
-    this.props.clearResults();
-  }
+    componentWillUnmount() {
+        this.props.clearResults();
+    }
 
-  render() {
-    let projects;
-    if (this.props.searchList && this.props.results) {
-      projects = Object.values(this.props.results).map((project, idx) => {
+    render() {
+        let projects;
+        if (this.props.searchList && this.props.results) {
+            projects = Object.values(this.props.results).map((project, idx) => {
+                return (
+                    <li key={idx} className="search-proj-li">
+                        <Link
+                            onClick={this.props.toggleSearch}
+                            to={`/projects/${project.id}`}
+                        >
+                            <div className="search-proj-img">
+                                <img src={project.photo} alt="" />
+                            </div>
+                            <div id="search-text">
+                                <h3>{project.name}</h3>
+                                <h3> By {project.creator.name}</h3>
+                            </div>
+                        </Link>
+                    </li>
+                );
+            });
+        } else if (
+            this.props.searchList &&
+            Object.values(this.props.results).length === 0
+        ) {
+            projects = <li className="search-proj-li">No matches found</li>;
+        } else {
+            projects = null;
+        }
+        if (!this.props.search) {
+            return <div className="hidden" />;
+        }
         return (
-          <li key={idx} className="search-proj-li">
-            <Link
-              onClick={this.props.toggleSearch}
-              to={`/projects/${project.id}`}
-            >
-              <div className="search-proj-img">
-                <img src={project.photo} alt="" />
-              </div>
-              <div id="search-text">
-                <h3>{project.name}</h3>
-                <h3> By {project.creator.name}</h3>
-              </div>
-            </Link>
-          </li>
+            <div className="search-bar">
+                <form onSubmit={this.handleSubmit}>
+                    <div className="seach-div">
+                        <input
+                            className="search-input"
+                            type="text"
+                            value={this.state.query}
+                            placeholder="search for a project by name"
+                            onChange={this.edit("query")}
+                        />
+                        <button
+                            id="close-search"
+                            type="button"
+                            onClick={this.props.toggleSearch}
+                        >
+                            X
+                        </button>
+                    </div>
+                </form>
+                <div
+                    className={this.props.searchList ? "search-list" : "hidden"}
+                >
+                    <ul className="search-list-ul">{projects}</ul>
+                </div>
+            </div>
         );
-      });
-    } else if (
-      this.props.searchList &&
-      Object.values(this.props.results).length === 0
-    ) {
-      projects = <li className="search-proj-li">No matches found</li>;
-    } else {
-      projects = null;
     }
-    if (!this.props.search) {
-      return <div className="hidden" />;
-    }
-    return (
-      <div className="search-bar">
-        <form onSubmit={this.handleSubmit.bind(this)}>
-          <div className="seach-div">
-            <input
-              className="search-input"
-              type="text"
-              value={this.state.query}
-              placeholder="search for a project by name"
-              onChange={this.edit("query")}
-            />
-            <button
-              id="close-search"
-              type="button"
-              onClick={this.props.toggleSearch}
-            >
-              X
-            </button>
-          </div>
-        </form>
-        <div className={this.props.searchList ? "search-list" : "hidden"}>
-          <ul className="search-list-ul">{projects}</ul>
-        </div>
-      </div>
-    );
-  }
 }
 
 export default connect(
-  msp,
-  mdp
+    msp,
+    mdp
 )(SearchBar);
